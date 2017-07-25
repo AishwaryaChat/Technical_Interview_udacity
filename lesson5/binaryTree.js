f00f9fe15ef9c672cee291661f6f6c01d771d473
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-07-25 15:15:54 +0530

    Add search methd to search and element in binary tree

--- lesson5/binaryTree.js
+++ lesson5/binaryTree.js
@@ -17,9 +17,19 @@ BinaryTree.prototype.traverse = function (start, traversal) {
   return traversal
 }
 
+BinaryTree.prototype.search = function (start, value) {
+  if (start) {
+    if (start.value === value) return 'Found'
+    return this.search(start.left, value) || this.search(start.right, value)
+  }
+  return 'Did not found'
+}
+
 const tree = new BinaryTree(1)
 tree.root.left = new Node(2)
 tree.root.right = new Node(3)
 tree.root.left.left = new Node(4)
 tree.root.left.right = new Node(5)
 console.log(tree.traverse(tree.root, ''))
+console.log(tree.search(tree.root, 4))
+console.log(tree.search(tree.root, 9))

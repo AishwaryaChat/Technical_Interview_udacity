5b25eb6cfab799bbea2f7560f141457142dc06d3
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-07-25 12:28:49 +0530

    Add store function to store a hash key

--- lesson4/hashTable.js
+++ lesson4/hashTable.js
@@ -1,5 +1,8 @@
 function HashTable () {
-  this.table = [null] * 1000
+  this.table = []
+  for (var i = 0; i < 10000; i++) {
+    this.table[i] = null
+  }
 }
 
 HashTable.prototype.calculateHashValue = function (key) {
@@ -7,6 +10,18 @@ HashTable.prototype.calculateHashValue = function (key) {
   return hashValue
 }
 
+HashTable.prototype.storeKey = function (key) {
+  let hashValue = this.calculateHashValue(key)
+  if (hashValue !== -1) {
+    if (this.table[hashValue] !== null) this.table[hashValue].push(key)
+    else this.table[hashValue] = [key]
+  }
+}
+
 let hashTable = new HashTable()
 
-console.log(hashTable.calculateHashValue('UDACITY'))
+// console.log(hashTable.calculateHashValue('UDACITY'))
+
+console.log(hashTable.storeKey('UDACITY'))
+console.log(hashTable.storeKey('UDACIOUS'))
+console.log(hashTable.table[8568])

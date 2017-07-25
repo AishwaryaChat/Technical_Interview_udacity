7b45c3ed3e03a1621b02852c21546037ff4eea98
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-07-25 15:53:16 +0530

    Add insert function for binary search tree

--- lesson5/binarySearchTree.js
+++ lesson5/binarySearchTree.js
@@ -8,6 +8,16 @@ function BinarySearchTree (root) {
   this.root = new Node(root)
 }
 
+BinarySearchTree.prototype.insert = function (start, newVal) {
+  if (newVal > start.value) {
+    if (start.right) this.insert(start.right, newVal)
+    else start.right = new Node(newVal)
+  } else if (newVal < start.value) {
+    if (start.left) this.insert(start.left, newVal)
+    else start.left = new Node(newVal)
+  } else console.log('Element already exist in the tree, cannot be inserted.')
+}
+
 BinarySearchTree.prototype.traverse = function (start, traversal) {
   if (start) {
     traversal = traversal + start.value + ' '
@@ -20,4 +30,8 @@ BinarySearchTree.prototype.traverse = function (start, traversal) {
 const bst = new BinarySearchTree(4)
 bst.root.left = new Node(3)
 bst.root.left.left = new Node(2)
+bst.insert(bst.root, 5)
+bst.insert(bst.root, 6)
+bst.insert(bst.root, 2)
+console.log(bst)
 console.log(bst.traverse(bst.root, ''))
